7c039480bc9b02b7e4d3cfa76063bf008f619544
Malin Andersson <annamalinelisabet@gmail.com>
2022-12-07 09:06:31 +0100

    ready to merge

--- src/Components/BookSection/BookSection.jsx
+++ src/Components/BookSection/BookSection.jsx
@@ -8,7 +8,7 @@ const BookSection = ({restaurant}) => {
         <div className="text-div">
             <h1 className={`header ${restaurant.color}`}>zushi {restaurant.title}</h1>
             <p>På ett enkelt opretentiöst men utsökt, iögonfallande och representativt sätt skapar vi en känsla av lyx, direkt till dig! Var god och fråga om vad du ska välja och vilka drycker som passar bäst med de rätter du har valt.</p>
-            <button className='button'>
+            <button className='btn btn-book'>
                 BOKA BORD
                 <div className="horizontal"></div>
                 <div className="vertical"></div>
